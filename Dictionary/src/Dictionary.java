de47d3c151f3de78fcf238515f3c967529446dc7
njuyang14 <yangx141220127@gmail.com>
2016-10-23 01:14:00 +0800

    finish Jlist association panel refresh, next to errorFix function

--- Dictionary/src/Dictionary.java
+++ Dictionary/src/Dictionary.java
@@ -6,6 +6,8 @@ public class Dictionary {
 	private ArrayList<String> wordMeaning = new ArrayList<String>();
 	private int len;
 	
+	public ArrayList currentList = new ArrayList();//保存当前找到的联想的单词
+	
 	public Dictionary(){
 		Scanner input = null;
 		try{
@@ -20,10 +22,17 @@ public class Dictionary {
 			wordMeaning.add(token[1]);
 		}
 		len = wordList.size();
+		for(int i=0;i<len;i++){
+			currentList.add(i);
+		}
 	}
 	
 	public int getSize(){
 		return len;
+	}
+	
+	public String getWord(int index){
+		return wordList.get(index);
 	}
 	
 	public String getMeaning(int index){
@@ -34,6 +43,17 @@ public class Dictionary {
 		String[] stockArr = new String[wordList.size()];
 		stockArr = wordList.toArray(stockArr);
 		return stockArr;
+	}
+	
+	public int[] getCurrentList(){
+		return currentList;
+	}
+	//从数组中获得当前显示于jlist中的String[]
+	public String[] getAnListOfCurrentArray(int[] a){
+		String[] current = null;
+		for(int i=0;i<a.length;i++)
+			current[i]=wordList.get(i);
+		return current;
 	}
 	
 	public int binarySearchWord(String searchWord){
@@ -49,5 +69,24 @@ public class Dictionary {
 		             low=mid+1;
 		     }
 		     return -1;
+	}
+	
+	public String[] hasSamePrefix(String prefix){
+		currentList.clear();
+		ArrayList<String> search = new ArrayList<String>();
+		int prefixLen = prefix.length();
+		for(int i = 0;i < len;i++){
+			if(prefixLen <= wordList.get(i).length()){
+			    String subStr = wordList.get(i).substring(0, prefixLen);
+			    if(subStr.equals(prefix)){
+			    	search.add(wordList.get(i));
+			    	currentList.add(i);
+			    }
+			}
+		}
+				
+		String[] stockArr = new String[search.size()];
+		stockArr = search.toArray(stockArr);
+		return stockArr;
 	}
 }
